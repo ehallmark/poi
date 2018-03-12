6692f22f40ad0e41f0ae9271d0991f70893d38df
ehallmark <ehallmark@gttgrp.com>
2018-03-11 19:30:36 -0700

    c

--- src/main/java/predict/Database.java
+++ src/main/java/predict/Database.java
@@ -20,7 +20,6 @@ public class Database {
     public static final String COORDINATES = "coordinates";
     public static final String TITLE = "title";
     public static final File dataFile = new File("coordinate_data.jobj");
-    public static final File poiToLocationsFile = new File("poi_to_locations.jobj");
     @Getter
     private Map<String,Map<String,Object>> data;
     @Getter @Setter
@@ -282,15 +281,14 @@ public class Database {
         final List<Map<String,Collection<String>>> nonPlaceMaps = Collections.synchronizedList(new ArrayList<>(allDataMaps));
 
         nonPlaceMaps.remove(populatedPlaceToLocationsMap);
-        Collection<PointOfInterest> poiToLocations = Collections.synchronizedSet(new HashSet<>());
+        Collection<String> poiToLocations = Collections.synchronizedSet(new HashSet<>());
         AtomicLong cnt = new AtomicLong(0);
         final long totalCnt = nonPlaceMaps.stream().mapToLong(n->n.size()).sum();
         database.setPois(database.getPois().stream().filter(poi->populatedPlaceToLocationsMap.containsKey(poi.getTitle())).collect(Collectors.toList()));
 
         nonPlaceMaps.parallelStream().forEach(map->{
             map.forEach((title,v)->{
-                PointOfInterest poi = database.getPoi(title);
-                poiToLocations.add(poi);
+                poiToLocations.add(title);
                 if(cnt.getAndIncrement()%100000==99999) {
                     System.out.println("Found "+cnt.get()+" out of "+totalCnt);
                 }
@@ -341,6 +339,5 @@ public class Database {
         //Map<String,Collection<String>> groupedPopulatedPlaces = groupMaps(populatedPlaceToLocationsMap,Arrays.asList(cityToLocationsMap,touristAttractionsToLocationsMap,countyToLocationsMap,villageToLocationsMap,districtToLocationsMap,villageToLocationsMap,municipalityToLocationsMap,formerMunicipalityToLocationsMap));
         //System.out.println("Matched grouped places: "+groupedPopulatedPlaces.size());
 
-        saveObject(poiToLocations,poiToLocationsFile);
   }
 }
