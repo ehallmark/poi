41ce1405b3c4b66dfeff96ea1fda7b34e121d895
ehallmark <ehallmark@gttgrp.com>
2018-04-15 20:36:43 -0700

    c

--- src/main/java/predict/tech_tags/LearnTechnologyStatistics.java
+++ src/main/java/predict/tech_tags/LearnTechnologyStatistics.java
@@ -1,11 +1,12 @@
 package main.java.predict.tech_tags;
 
+import main.java.predict.Database;
 import main.java.util.StopWords;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.NDArrayIndex;
 
-import java.io.BufferedInputStream;
-import java.io.EOFException;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
@@ -17,18 +18,21 @@ public class LearnTechnologyStatistics {
         return text.toLowerCase().replaceAll("[^a-z ]"," ").split("\\s+");
     };
 
+    public static final File matrixFile = new File("tech_tag_statistics_matrix.jobj");
     public static void main(String[] args) throws Exception {
         final Set<String> stopWords = new HashSet<>(StopWords.getStopWords());
         final int minVocabSize = 3;
         final int vocabLimit = 40000;
         Map<String,AtomicLong> vocabCount = new HashMap<>();
         Map<String,AtomicLong> docCount = new HashMap<>();
+        Set<String> allTitles = new HashSet<>();
         Consumer<CategoryWithText> vocabConsumer = category -> {
             String[] words = textToWordFunction.apply(category.getText());
             if(words.length<3) return;
             for(int i = 0; i < 3; i++) {
                 if(words[i].equals("redirect")) return;
             }
+            allTitles.add(category.getTitle());
             for(String word : words) {
                 if(!stopWords.contains(word)) {
                     vocabCount.putIfAbsent(word, new AtomicLong(0));
@@ -54,21 +58,53 @@ public class LearnTechnologyStatistics {
                 })
                 .filter(e->e.getValue().get()>minVocabSize)
                 .limit(vocabLimit)
-                .map(e->{
-                    System.out.println("Score for "+e.getKey()+": "+((double)e.getValue().get())/Math.log(1f+docCount.get(e.getKey()).get()));
-                    return e;
-                })
                 .collect(Collectors.toMap(e->e.getKey(),e->e.getValue()));
 
 
         System.out.println("Vocab size before: "+vocabCount.size());
         System.out.println("Vocab size after: "+filteredVocabCount.size());
 
+        Map<String,Integer> titleToIndexMap = new HashMap<>();
+        Map<String,Integer> wordToIndexMap = new HashMap<>();
+
+        List<String> allWordsList = new ArrayList<>(filteredVocabCount.keySet());
+        allWordsList.sort(Comparator.naturalOrder());
+        List<String> allTitlesList = new ArrayList<>(allTitles);
+        allTitlesList.sort(Comparator.naturalOrder());
+
+        for(int i = 0; i < allWordsList.size(); i++) {
+            wordToIndexMap.put(allWordsList.get(i),i);
+        }
+        for(int i = 0; i < allTitlesList.size(); i++) {
+            titleToIndexMap.put(allTitlesList.get(i),i);
+        }
+
+        final INDArray coocurrenceMatrix = Nd4j.zeros(allTitles.size(),filteredVocabCount.size());
         Consumer<CategoryWithText> mainConsumer = category -> {
+            String title = category.getTitle();
+            Integer titleIdx = titleToIndexMap.get(title);
+            if(titleIdx==null) return;
+            String[] words = textToWordFunction.apply(category.getText());
+            if(words.length<3) return;
+            for(int i = 0; i < 3; i++) {
+                if(words[i].equals("redirect")) return;
+            }
+
+            for(String word : words) {
+                Integer wordIdx = wordToIndexMap.get(word);
+                if(wordIdx!=null) {
+                    coocurrenceMatrix.get(NDArrayIndex.point(titleIdx),NDArrayIndex.point(wordIdx)).addi(1d);
+                }
+            }
 
         };
         // main pass
         iterate(mainConsumer);
+
+        // save weights
+        coocurrenceMatrix.diviColumnVector(coocurrenceMatrix.norm2(1));
+
+        Database.saveObject(coocurrenceMatrix,matrixFile);
     }
 
     public static void iterate(Consumer<CategoryWithText> consumer) {
