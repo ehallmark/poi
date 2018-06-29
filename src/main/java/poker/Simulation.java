d1660cfb3e0b6e11b7e049da2ac5d0cdfaa77991
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-29 11:34:57 -0700

    c

--- src/main/java/poker/Simulation.java
+++ src/main/java/poker/Simulation.java
@@ -1,7 +1,10 @@
 package main.java.poker;
 
+import javax.smartcardio.Card;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Simulation {
     private static final Random rand = new Random(2352);
@@ -9,19 +12,17 @@ public class Simulation {
     private List<Card> deck;
     private AtomicInteger deckIdx;
     private int numPlayers;
-    public Simulation(int numDecks, int numPlayersStart) {
+    public Simulation(int numPlayersStart) {
         this.numPlayersStart=numPlayersStart;
         this.numPlayers=numPlayersStart;
         deckIdx = new AtomicInteger(0);
-        deck = new ArrayList<>(52*numDecks);
-        for(int i = 0; i < numDecks; i++) {
+        deck = new ArrayList<>(52);
+        for(char c : new char[]{'C', 'D', 'H', 'S'}) {
             for(int j = 1; j <= 13; j++) {
-                for(char c : new char[]{'c', 'd', 'h', 's'}) {
-                    Card card = new Card();
-                    card.num=j;
-                    card.suit=c;
-                    deck.add(card);
-                }
+                Card card = new Card();
+                card.num=j;
+                card.suit=c;
+                deck.add(card);
             }
         }
     }
@@ -48,9 +49,126 @@ public class Simulation {
             hand.cards = new Card[]{nextCard(), nextCard()};
             hands[i]=hand;
         }
+        nextCard(); // burn
+        Card[] flop = new Card[]{nextCard(), nextCard(), nextCard()};
+        nextCard(); // burn
+        Card turn = nextCard();
+        nextCard(); // burn
+        Card river = nextCard();
+
         return hands;
     }
 
+
+    public boolean containsFlush(Card[] cards) {
+        Set<Character> suits = new HashSet<>();
+        for(Card card : cards) {
+            suits.add(card.suit);
+        }
+        return suits.size()==1;
+    }
+
+    public boolean containsStraight(Card[] cards) {
+        int min = 14;
+        int max = 0;
+        Set<Integer> nums = new HashSet<>();
+        for(Card card : cards) {
+            if(card.num<min) {
+                min = card.num;
+            }
+            if(card.num>max) {
+                max = card.num;
+            }
+            nums.add(card.num);
+        }
+        boolean straight = max-min==5&&nums.size()==5;
+        if(!straight) {
+            // need to check aces over kings
+            min = 14;
+            max = 0;
+            nums = new HashSet<>();
+            for(Card card : cards) {
+                int cardNum = card.num;
+                if(cardNum==1) cardNum=14;
+                if(cardNum<min) {
+                    min = cardNum;
+                }
+                if(cardNum>max) {
+                    max = cardNum;
+                }
+                nums.add(cardNum);
+            }
+            straight = max-min==5&&nums.size()==5;
+        }
+        return straight;
+    }
+
+
+
+    public int largestNOfAKind(Card[] cards) {
+        Map<Integer,Long> counts = Stream.of(cards).collect(Collectors.groupingBy(e->e.num, Collectors.counting()));
+        return counts.values().stream().mapToInt(d->d.intValue()).max().orElse(1);
+    }
+
+    public boolean containsFullHouse(Card[] cards) {
+        Map<Integer,Long> counts = Stream.of(cards).collect(Collectors.groupingBy(e->e.num, Collectors.counting()));
+        return counts.size()==2 && Arrays.asList(2,3).contains(counts.get(cards[0].num));
+    }
+
+    public boolean containsTwoPair(Card[] cards) {
+        Map<Integer,Long> counts = Stream.of(cards).collect(Collectors.groupingBy(e->e.num, Collectors.counting()));
+        return counts.size()==3 && counts.values().stream().mapToInt(d->d.intValue()).max().orElse(1)==2;
+    }
+
+
+
+    public double scoreHand(Card[] cards) {
+        if(cards.length!=5) throw new RuntimeException("Can only score 5 cards at a time.");
+        boolean flush = containsFlush(cards);
+        boolean straight = containsStraight(cards);
+        double score = 0d;
+        if(flush && straight) {
+            // straight flush!
+            score = Math.max(score, 8d);
+        } else if (flush) {
+            score = Math.max(score, 5d);
+        } else if (straight) {
+            score = Math.max(score, 4d);
+        }
+        int largestN  = largestNOfAKind(cards);
+        if(largestN==4) {
+            // 4 of a kind
+            score = Math.max(score, 7d);
+        } else if (largestN==3) {
+            // check for full house
+            boolean fullHouse = containsFullHouse(cards);
+            if(fullHouse) {
+                score = Math.max(score, 6d);
+
+            } else {
+                // 3 of a kind
+                score = Math.max(score, 3d);
+            }
+        } else if (largestN==2) {
+            // check 2 pair
+            boolean twoPair = containsTwoPair(cards);
+            if(twoPair) {
+                // two pair
+                score = Math.max(score, 2d);
+            } else {
+                // single pair
+                score = Math.max(score, 1d);
+            }
+        } else {
+            score = Math.max(score, 0d);
+        }
+
+        return score;
+    }
+
+
+
+    public static double probabilityOfHand()
 
 
     class Hand {
@@ -66,13 +184,12 @@ public class Simulation {
         char suit;
         @Override
         public String toString() {
-            return String.valueOf(num)+String.valueOf(suit).toUpperCase();
+            return String.valueOf(num)+String.valueOf(suit);
         }
     }
 
-
     public static void main(String[] args) {
-        Simulation simulation = new Simulation(4, 5);
+        Simulation simulation = new Simulation(5);
         Hand[] hands = simulation.simulateHand();
         for(Hand hand : hands) {
             System.out.println("Hand: "+hand);
