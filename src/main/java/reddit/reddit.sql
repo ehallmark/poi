b19025f459c648333fb83957d020f792881e7d70
ehallmark <ehallmark@gttgrp.com>
2018-09-20 11:03:29 -0700

    c

--- src/main/java/reddit/reddit.sql
+++ src/main/java/reddit/reddit.sql
@@ -41,11 +41,30 @@ insert into comment_comments (
     select c.id,c.parent_id,c.subreddit_id,c.link_id,c.text,c.score,c.ups,c.author,c.controversiality,
         p.link_id,p.text,p.score,p.ups,p.author,p.controversiality
     from comments as c join comments as p on (c.parent_id='t1_'||p.id)
+    where c.score > 50 or c.score < -5
 );
 
 create index reddit_comment_comments_parent_id_idx on comment_comments (parent_id);
 create index reddit_comment_comments_subreddit_id_idx on comment_comments (subreddit_id);
 
 
-\copy (select * from comment_comments where score < 1 or controversiality > 0 order by random() limit 10000000) to /home/ehallmark/Downloads/comment_comments0.csv delimiter ',' csv header;
-\copy (select * from comment_comments where score > 50 and controversiality = 0 order by random() limit 10000000) to /home/ehallmark/Downloads/comment_comments1.csv delimiter ',' csv header;
+\copy (select * from comment_comments order by controversiality desc, score asc, random() limit 1100000) to /home/ehallmark/Downloads/comment_comments0.csv delimiter ',' csv header;
+\copy (select * from comment_comments where controversiality = 0 order by score desc, random() limit 1100000) to /home/ehallmark/Downloads/comment_comments1.csv delimiter ',' csv header;
+
+
+create table comments_words (
+    word text primary key,
+    docs_appeared_in double precision not null,
+    pos_docs_appeared_in double precision not null,
+    neg_docs_appeared_in double precision not null
+);
+
+insert into comments_words (
+    select word, count(*),
+    sum(case when controversiality = 1 or score < 0 then 1 else 0 end),
+    sum(case when controversiality != 1 and score > 0 then 1 else 0 end)
+    from comment_comments, unnest(string_to_array(regexp_replace(lower(text), '[^a-z ]', ' ', 'g'),' ')) as w(word)
+    group by word
+    having count(*) > 100
+);
+
